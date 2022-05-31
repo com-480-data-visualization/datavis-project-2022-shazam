d0b57df77fc3bd4e6748f7dc2bdaca52e379aee4
Chun-Hung Tseng <henrybear327@gmail.com>
2022-06-01 01:33:40 +0200

    Clean up

--- dataviz/pages/index.vue
+++ dataviz/pages/index.vue
@@ -9,14 +9,12 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import Tutorial from '~/components/Tutorial.vue'
 import RightLineChart from '~/components/RightLineChart.vue'
 import Navbar from '../components/navbar.vue'
 
 export default Vue.extend({
     name: "IndexPage",
     components: { 
-      Tutorial: Tutorial,
       RightLineChart: RightLineChart,
       Navbar: Navbar,
     }
