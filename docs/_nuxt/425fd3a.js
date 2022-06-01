599b58e0d47789e97a41f0f11094d1659a987bb5
Chun-Hung Tseng <henrybear327@gmail.com>
2022-06-01 11:35:50 +0200

    Generate static site

---
+++ docs/_nuxt/425fd3a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{132:function(n,o,e){"use strict";var t=e(2),w=e(177),c=e.n(w),d=e(178),f=e.n(d),r=e(100),k=e.n(r);f()(k.a),t.default.use(c.a)},181:function(n,o,e){e(182),n.exports=e(183)}},[[181,14,1,15]]]);
