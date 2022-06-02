4a70dd97fe79c775bd8d7596ce9bee2979dde857
Chun-Hung Tseng <henrybear327@gmail.com>
2022-06-02 04:09:23 +0200

    Generate static site

--- docs/_nuxt/static/1654128334/timeline/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdatavis-project-2022-shazam\u002F_nuxt\u002Fstatic\u002F1654128334",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftimeline",config:{_app:{basePath:"\u002Fdatavis-project-2022-shazam\u002F",assetsPath:"\u002Fdatavis-project-2022-shazam\u002F_nuxt\u002F",cdnURL:a}}}}(null));
