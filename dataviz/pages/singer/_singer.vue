3f10c26be2972d0cf5994a3b3b47c43d03cc0faa
Everina <zimi.liu@epfl.ch>
2022-06-03 04:53:53 +0200

    Fix Singer Routing bug

---
+++ dataviz/pages/singer/_singer.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import Vue from 'vue'
+import singerComponentVue from '~/components/singerComponent.vue'
+
+export default Vue.extend({
+    name: "SingerPage",
+    components: { 
+      Singer: singerComponentVue
+    }
+})
+</script>
+
+<template>
+
+<div class="bg-gray-900 min-h-screen">
+
+  <navbar/> 
+  
+  <Singer/>
+  
+</div>
+
+</template>
