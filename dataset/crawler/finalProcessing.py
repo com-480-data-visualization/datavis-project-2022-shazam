ba60489d39f591820185361182104932e247f4cf
dyxcooh <yingxuan.duan@epfl.ch>
2022-06-03 05:45:20 +0200

    Update comments Change dataset for yearly to ALL songs Add new yearly dataset

--- dataset/crawler/finalProcessing.py
+++ dataset/crawler/finalProcessing.py
@@ -34,6 +34,8 @@ def load_qq_data(id: int) -> "dict[int, dict[int, dict[int, dict[str, str]]]]":
     folder=f"{base_folder}/{id}"
     
     ret = {}
+    ok = {}
+    failed = {}
     for y in range(2018, 2022 + 1):
         ret[y] = {}
         for w in range(1, 52 + 1):
@@ -49,20 +51,23 @@ def load_qq_data(id: int) -> "dict[int, dict[int, dict[int, dict[str, str]]]]":
                         ret[y][w][i]['title'] = track['title']
                         ret[y][w][i]['singerName'] = track['singerName']
                         ret[y][w][i]['mid'] = track['mid']
-                        # try:
-                        #     mid = track['mid']
-                        #     title = track['title']
-                        #     title = title.replace("/", " ")
-                        #     with open(f'{base_folder}/lyrics/{mid}_{title}.json', encoding='utf-8') as lyrics_file:
-                        #         data = json.load(lyrics_file)
-                        #         str_without_brackets = re.sub("[\(\[].*?[\)\]]", "", data['data']['lyric']) # TODO: remove more terms from the first couple of lines
-                        #         ret[y][w][i]['lyrics'] = str_without_brackets
-                        # except Exception as e:
-                        #     print(e, f'{base_folder}/lyrics/{mid}_{title}.json')
-                        #     pass
+                        
+                        # get audio features
+                        mid = track['mid']
+                        try:
+                            with open(f'data/tracks/{mid}.json', encoding='utf-8') as infile2:
+                                audioFeatures = json.load(infile2)
+                                ret[y][w][i]['audioFeatures'] = audioFeatures
+                                print(f'Found {folder}/{y}_{w}.json')
+                                ok[mid] = True
+                        except Exception as e:
+                            print(e, f'{folder}/{y}_{w}.json')
+                            failed[mid] = True
+                            pass
             except Exception as e:
                 print(e, f'{folder}/{y}_{w}.json')
                 pass
+    print(len(ok), len(failed))
     return ret
 
 print("Start loading trend_data_billboard")
@@ -71,11 +76,7 @@ print("Done loading trend_data_billboard")
 
 cutoffCount = 20
 
-# for each week of the billboard data
-# we take the top x of the tracks
-# 1) aggregate the singer data out
-# 2) aggregate the track data out
-# cutoff: take the top x (inclusive) singers
+# get a list of available singers
 def aggregateSingers(data: "dict[int, dict[int, dict[int, dict[str, str]]]]", cutoff: int) -> "list[str]":
     distinctSingers = {}
     for y in data:
@@ -102,7 +103,7 @@ def aggregateSingers(data: "dict[int, dict[int, dict[int, dict[str, str]]]]", cu
 distinctSingers = aggregateSingers(data=trend_data_billboard, cutoff=cutoffCount)
 print(cutoffCount, len(distinctSingers))
 
-# calculates trakcs per singer in that week
+# calculates distinct singers - track count in that week
 def generateBubbleChartSingerData(data: "dict[int, dict[int, dict[int, dict[str, str]]]]", cutoff: int) -> "list[str]":
     for y in data:
         for w in data[y]:
@@ -134,8 +135,8 @@ def generateBubbleChartSingerData(data: "dict[int, dict[int, dict[int, dict[str,
             
 generateBubbleChartSingerData(data=trend_data_billboard, cutoff=cutoffCount)
 
-# calculates accumulated for all singers, total distinct tracks, per week
-def generateBarChartAccumulatedSingerHistoryData(data: "dict[int, dict[int, dict[int, dict[str, str]]]]", cutoff: int) -> "list[str]":
+# calculates accumulated for all singers, total distinct tracks, for every week since 2018
+def generateBarChartAccumulatedSingerHistoryDataPerWeek(data: "dict[int, dict[int, dict[int, dict[str, str]]]]", cutoff: int) -> "list[str]":
     tracks = {} # singer -> {track name: 1}
     for y in data:
         for w in data[y]:
@@ -173,4 +174,97 @@ def generateBarChartAccumulatedSingerHistoryData(data: "dict[int, dict[int, dict
             with open(f'data/singers/distinct_historical/{y}_{w}.json', 'w') as outfile:
                 outfile.write(json_string)
             
-generateBarChartAccumulatedSingerHistoryData(data=trend_data_billboard, cutoff=cutoffCount)
+generateBarChartAccumulatedSingerHistoryDataPerWeek(data=trend_data_billboard, cutoff=cutoffCount)
+
+# calculates accumulated for all singers, total distinct tracks, per year
+def generateBarChartAccumulatedSingerHistoryDataPerYear(data: "dict[int, dict[int, dict[int, dict[str, str]]]]", cutoff: int) -> "list[str]":
+    tracks = {} # singer -> {track name: 1}
+    for y in data:
+        for w in data[y]:
+            for i in data[y][w]:
+                if i < cutoff:
+                    singers = data[y][w][i]['singerName']
+                    trackName = data[y][w][i]['title']
+                    singerList = singers.split("/")
+                    for singer in singerList:
+                        if singer in tracks:
+                            if trackName not in tracks[singer]:
+                                tracks[singer][trackName] = 1
+                            else:
+                                val = tracks[singer][trackName]
+                                val += 1
+                                tracks[singer][trackName] = val
+                        else:
+                            tracks[singer] = {trackName: 1}
+            
+        singerData = []
+        for k, v in tracks.items():
+            singerData.append((k, len(v)))
+        singerData.sort(reverse=True, key=lambda element: (element[1], element[0]))
+
+        # save it
+        output = []
+        for k, v in singerData:
+            output.append({
+                'name': k,
+                'count': v,
+            })
+
+        json_string = json.dumps(output)
+        os.system(f"mkdir -p data/singers/distinct_historical")
+        with open(f'data/singers/distinct_historical/{y}.json', 'w') as outfile:
+            outfile.write(json_string)
+            
+generateBarChartAccumulatedSingerHistoryDataPerYear(data=trend_data_billboard, cutoff=100)
+
+# calculates the weeks that the songs stayed on the board
+def generateBarChartAccumulatedSongsHistoryDataPerYear(data: "dict[int, dict[int, dict[int, dict[str, str]]]]", cutoff: int) -> "list[str]":
+    tracks = {} # track -> singer, count
+    for y in data:
+        for w in data[y]:
+            for i in data[y][w]:
+                if i < cutoff:
+                    trackName = data[y][w][i]['title']
+                    singers = data[y][w][i]['singerName']
+                    if trackName not in tracks:
+                        tracks[trackName] = (1, singers)
+                    else:
+                        val = tracks[trackName]
+                        tracks[trackName] = (val[0] + 1, val[1])
+            
+        trackData = []
+        for k, v in tracks.items():
+            trackData.append((k, v))
+        trackData.sort(reverse=True, key=lambda element: (element[1][0], element[0]))
+
+        # save it
+        output = []
+        for k, v in trackData:
+            output.append({
+                'name': k,
+                'count': v[0],
+                'singer': v[1],
+            })
+
+        json_string = json.dumps(output)
+        os.system(f"mkdir -p data/tracks/historical")
+        with open(f'data/tracks/historical/{y}_weeks.json', 'w') as outfile:
+            outfile.write(json_string)
+            
+generateBarChartAccumulatedSongsHistoryDataPerYear(data=trend_data_billboard, cutoff=100)
+
+# audio features of all tracks this year# calculates the weeks that the songs stayed on the board
+def generateAudioFeaturesPerYear(data: "dict[int, dict[int, dict[int, dict[str, str]]]]", cutoff: int) -> "list[str]":
+    for y in data:
+        tracks = []
+        for w in data[y]:
+            for i in data[y][w]:
+                if i < cutoff:
+                    tracks.append(data[y][w][i])
+        
+        json_string = json.dumps(tracks)
+        os.system(f"mkdir -p data/tracks/historical")
+        with open(f'data/tracks/historical/{y}_audio_features.json', 'w') as outfile:
+            outfile.write(json_string)
+            
+generateAudioFeaturesPerYear(data=trend_data_billboard, cutoff=100)
